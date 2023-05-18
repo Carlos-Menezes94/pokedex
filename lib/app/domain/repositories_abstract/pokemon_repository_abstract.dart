92b97def63fb61ef89071556b47006c9798fcc4c
Rafael <menezes.carloseng@gmail.com>
2023-05-18 10:20:48 -0300

    Create repository abstract

---
+++ lib/app/domain/repositories_abstract/pokemon_repository_abstract.dart
@@ -0,0 +1,9 @@
+import 'package:either_dart/either.dart';
+
+import '../../../core/failure.dart';
+import '../../data/models/pokemon_model.dart';
+
+abstract class PokemonRepositoryAbstract {
+  Future<Either<Failure, PokemonModel>> getPokemonsList();
+
+}
