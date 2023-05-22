711623767036b33a18eb45674ade3419106a3b61
Rafael <menezes.carloseng@gmail.com>
2023-05-22 09:45:11 -0300

    Change in file name and import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'app/depedency_injector/di_container.dart';
-import 'app/presentation/pages/home_page.dart';
+import 'app/presentation/pages/pokemon_list_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
